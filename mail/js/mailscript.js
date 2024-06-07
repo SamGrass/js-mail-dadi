40ee0900f52259bac30ffc06752b19ecf242513a
Samgrass <samuelegrassetti01@gmail.com>
2024-06-07 13:47:41 +0200

    add items + add email input add declared variables

---
+++ mail/js/mailscript.js
@@ -0,0 +1,16 @@
+// creo lista mail da accettare
+const approvedList = ["luigi@gmail.com", "beppe@gmail.com", "lorenzo@gmail.com", "marco@gmail.com"];
+
+// Chiedi all’utente la sua email 
+let userButton = document.querySelector("button");
+let userMail = document.querySelector(".mail")
+
+
+
+
+
+
+
+
+// stampa un messaggio appropriato sull’esito del controllo.
+
