3e9f99d1184360edafcfb1d053d013b7e9432930
Samgrass <samuelegrassetti01@gmail.com>
2024-06-10 16:08:53 +0200

    add js email check

--- mail/js/mailscript.js
+++ mail/js/mailscript.js
@@ -1,16 +1,34 @@
 // creo lista mail da accettare
 const approvedList = ["luigi@gmail.com", "beppe@gmail.com", "lorenzo@gmail.com", "marco@gmail.com"];
 
+console.log(approvedList);
 // Chiedi all’utente la sua email 
 let userButton = document.querySelector("button");
 let userMail = document.querySelector(".mail")
 
 
+console.log(userButton, userMail);
+userButton.addEventListener("click",
+    function () {
+        let inputMail = userMail.value;
+
+        console.log(inputMail);
+
+        let result = "Accesso Negato";
 
 
+        // controlla che sia nella lista di chi può accedere stampa un messaggio appropriato sull’esito del controllo.
+        for (let i = 0; i < approvedList.length; i++) {
+            const access = approvedList[i];
+            
+            if (inputMail === access) {
+                result = "Accesso Consentito"
+            }
+        }
 
+        console.log(result);
+       
+    }
 
-
-
-// stampa un messaggio appropriato sull’esito del controllo.
+);
 
